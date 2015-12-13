e332230b1b02e542017c96d5e628244ff2274198
Tom <tom@pckls.io>
2015-12-13 18:57:38 +0800

    Fixing up some initial files

--- manifests/confd/puppetagent.pp
+++ manifests/confd/puppetagent.pp
@@ -4,7 +4,9 @@ class telegraf::confd::puppetagent {
         section    => 'plugins',
         subsection => 'puppetagent',
         subsection_conf => {
+            # TODO: Make this a variable
             location => "\"/var/lib/puppet/state/last_run_summary.yaml\"",
+            #/opt/puppetlabs/puppet/cache/state/last_run_summary.yaml
         },
     }
 
